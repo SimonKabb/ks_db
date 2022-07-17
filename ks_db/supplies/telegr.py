2b1c09a95ed743e5332dbd0e2077b7bbf8a27525
Семён Капустников <s.v.kapustnikov@gmail.com>
2022-07-17 23:55:12 +0300

    hide token

--- ks_db/supplies/telegr.py
+++ ks_db/supplies/telegr.py
@@ -1,10 +1,9 @@
 from telegram import Bot
+from .varriable import TOKEN, CHAT_ID
 
 
 def send_telegram(orders):
-    bot = Bot(token='5326387724:AAHfKe8Jw-orUM5ZS6VGTRf2Kk5RprMn-AM')
-    # Укажите id своего аккаунта в Telegram
-    chat_id = 76411159
+    bot = Bot(token=TOKEN)
+    chat_id = CHAT_ID
     text = 'Нарушение сроков поставки у данных заказов: ' + orders
-    # Отправка сообщения
     bot.send_message(chat_id, text)
